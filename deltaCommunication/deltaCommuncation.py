3db7396deaa55deaa6a26c5b07b2f524de454e0d
PiatekBartosz <bartekpiatek123@gmail.com>
2022-10-23 01:46:20 +0200

    Finished simulating delta communication, Todo combine with vision system

--- deltaCommunication/deltaCommuncation.py
+++ deltaCommunication/deltaCommuncation.py
@@ -2,12 +2,21 @@ import socket
 from threading import Thread
 from time import sleep
 
+"""
+    Boundaries for robot delta:
+        z belongs to [-7000, -4000]
+        for z == -4000 x and y belongs to [-3700, 3700]
+        for z == -7000 x and y belongs to [-3000, 3000]
+"""
+
+
 HOST, PORT = "localhost", 2137
 home_pos = "-2000-2000-4500"
 obj_hover_height = "-4500"
 obj_pickup_height = "-7000"
-error = 100  # how much can differ the real and set position
-queue = [(2500, 5300, 0), (2444, 2555, 1)]  # later will be replaced with vision system return (x, y, type_of)
+offset_threshold = 100  # how much can differ the real and set position
+sleep_time = 0.2  # how long in seconds will the G_P command be send while checking if achieved position
+queue = [(2500, 1300, 0), (2444, 2555, 1), (2444, 2565, 3), (2444, -1500, 2)]  # later will be replaced with vision system return (x, y, type_of)
 
 # put down location coordinates
 put_location_1 = "+1000+1000"
@@ -15,33 +24,68 @@ put_location_2 = "+1000-1000"
 put_location_3 = "-1000+1000"
 put_location_4 = "-1000-1000"
 
-global sock
-global current_position
-global running
-running = True
+
+def execute_command(command):
+    return_value = None
+    prefix = command[0:3]
+
+    if prefix == "G_P":
+        sock.send("G_P".encode())
+        recv = sock.recv(256).decode()
+        return recv
+
+    elif prefix == "LIN":
+        print("Performing: ", command)
+        set_x, set_y, set_z = get_coordinates(command)
+        sock.send(command.encode())
+        sleep(sleep_time)
+        while True:
+            sock.send("G_P".encode())
+            recv = sock.recv(256).decode()
+            curr_x, curr_y, curr_z = get_coordinates(recv)
+            print("Current position: ", curr_x, " ", curr_y, " ", curr_z, " ")
+
+            offsets = []
+            offsets.append(abs(set_x - curr_x))
+            offsets.append(abs(set_y - curr_y))
+            offsets.append(abs(set_z - curr_z))
+
+            if max(offsets) <= offset_threshold:
+                break
+            else:
+                sock.send(command.encode())
+            sleep(sleep_time)
+
+    elif prefix == "TIM":
+        timeout = command[3:6]
+        sleep(int(timeout)//1000)
+    elif prefix == "JNT":
+        pass
+
+    elif prefix == "CIR":
+        pass
+
+    sleep(sleep_time)
+    return
 
 
-def handle_communication():
-    while queue:
-        x, y, type_of = queue[0][0], queue[0][1], queue[0][2]
-        commands = create_commands(x, y, type_of)
-
-        while commands:
-            x_set, y_set, z_set = get_coordinates(commands[0])
-            sock.send(bytes(commands[0], "utf-8"))
-            wait_until_achieved_pos(x_set, y_set, z_set)
-            commands.pop(0)
-        queue.pop(-1)
-        print("end")
-    running = False
+# todo update to enable controlling the suction cup and differentiate between picking up and putting down
+def pick_up_command(coordinates):
+    pick_up = []
+    pick_up.append("LIN" + coordinates + obj_hover_height + "TOOL")
+    pick_up.append("LIN" + coordinates + obj_pickup_height + "TOOL")
+    pick_up.append("TIM" + str(2000))
+    pick_up.append("LIN" + coordinates + obj_hover_height + "TOOL")
+    return pick_up
 
 
-def get_pos():
-    while running:
-        sock.send(bytes("G_P", "utf-8"))
-        recv = get_coordinates(sock.recv(128), "utf-8")
-        current_position = recv
-        sleep(0.03)
+def putting_down_command(put_location):
+    put_down = []
+    put_down.append("LIN" + put_location + obj_hover_height + "TOOL")
+    put_down.append("LIN" + put_location + obj_pickup_height + "TOOL")
+    put_down.append("TIM" + str(2000))
+    put_down.append("LIN" + put_location + obj_hover_height + "TOOL")
+    return put_down
 
 
 def get_coordinates(s):
@@ -50,7 +94,6 @@ def get_coordinates(s):
     z = int(s[14:18]) if s[13] == "+" else int(s[13:18])
     # todo convert to number normalize x, y, z
     return x, y, z
-# parse x, y, z position from robot callbackz
 
 
 def create_commands(x, y, type_of):
@@ -78,48 +121,26 @@ def create_commands(x, y, type_of):
     return commands
 
 
-def pick_up_command(coordinates):
-    pick_up = []
-    pick_up.append("LIN" + coordinates + obj_hover_height + "TOOL")
-    pick_up.append("LIN" + coordinates + obj_pickup_height + "TOOL")
-    pick_up.append("LIN" + coordinates + obj_hover_height + "TOOL")
-    return pick_up
-# todo update to enable controlling the suction cup and differentiate between picking up and putting down
+def start():
+    global sock
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    sleep(1)
+    print("Connected to delta")
 
+    # go home
+    execute_command("LIN" + home_pos + "TOOL")
+    commands = []
+    for element in queue:
+        if abs(element[0]) > 3000 or abs(element[1] > 3000):
+            continue
+        commands.extend(create_commands(element[0], element[1], element[2]))
+    print(commands)
 
-def putting_down_command(put_location):
-    put_down = []
-    put_down.append("LIN" + put_location + obj_hover_height + "TOOL")
-    put_down.append("LIN" + put_location + obj_pickup_height + "TOOL")
-    put_down.append("LIN" + put_location + obj_hover_height + "TOOL")
-    return put_down
+    while commands:
+        execute_command(commands[0])
+        commands.pop(0)
 
-
-def wait_until_achieved_pos(x_set, y_set, z_set):
-    x_curr, y_curr, z_curr = current_position
-    while abs(x_curr - x_set) > error or abs(y_curr - y_set) > error or abs(z_curr - z_set) > error:
-        x_curr, y_curr, z_curr = current_position
-        sleep(0.01)
-    return
-
-
-#
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-
-print("Connected to delta")
-
-# set home position
-sock.sendall(bytes("LIN" + home_pos + "TOOL", "utf-8"))
-
-# init current position
-sock.sendall(bytes("G_P", "utf-8"))
-recv = str(sock.recv(128), "utf-8")
-current_position = get_coordinates(recv)
-
-# thread1 = Thread(target=handle_communication)
-thread2 = Thread(target=get_pos)
-
-# thread1.start()
-thread2.start()
+start()
+sleep(20)
 
