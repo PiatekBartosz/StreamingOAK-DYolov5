ff21591f4500dc32db608548406af77817992e40
bartek <bartekpiatek123@gmail.com>
2022-11-02 01:56:08 +0100

    Delta communication working but in an infinite loop

--- deltaCommunication/deltaCommunication2.py
+++ deltaCommunication/deltaCommunication2.py
@@ -5,6 +5,10 @@ import json
 import sys
 
 """
+     This program constantly updates queue for detected chocolate bars, and send move commands to robot delta 
+"""
+
+"""
     Boundaries for robot delta:
         z belongs to [-7000, -4000]
         for z == -4000 x and y belongs to [-3700, 3700]
@@ -12,10 +16,8 @@ import sys
 """
 
 # store predictions acquired by the vision system
-global queue, speed, thread_lock, queue_update_time
 queue = []
 queue_lock = threading.Lock()
-queue_update_time = 1
 
 """
 (x_pos, y_pos, type_of_chocolate_bar)
@@ -26,244 +28,254 @@ type_of_chocolate_bar:
     3 -> Snickers
 """
 
-
 # todo parse the arguments to see if we want to use simulation or real delta set global speed for all commands
 
-class Singleton:
-    __instance = None
-    def __new__(cls):
-        if cls.__instance is None:
-            # __new__ method static method, doesn't take self
-            cls.__instance = super(Singleton, cls).__new__(cls)
 
-        return cls.__instance
+"""
+    Init Vision system
+"""
 
-
-class DeltaClient(Singleton):
-    sock = None
-    HOST, PORT = "localhost", 2137
-    home_pos = "-2000-2000-4500"
-    obj_hover_height = "-4500"
-    obj_pickup_height = "-7000"
-    offset_threshold = 100  # how much can differ the real and set position
-    sleep_time = 1  # how long in seconds will the G_P command be send while checking if achieved position
-
-    # put down location coordinates
-    put_location_1 = "+1000+1000"
-    put_location_2 = "+1000-1000"
-    put_location_3 = "-1000+1000"
-    put_location_4 = "-1000-1000"
-
-    def __del__(self):
-        if self.sock:
-            self.sock.close()
-
-    def execute_command(self, command):
-        return_value = None
-        prefix = command[0:3]
-
-        if prefix == "G_P":
-            self.sock.send("G_P".encode())
-            recv = self.sock.recv(26).decode()
-            return recv
-
-        elif prefix == "LIN":
-            print("Performing: ", command)
-            set_x, set_y, set_z = self.get_coordinates(command)
-            self.sock.send(command.encode())
-            sleep(self.sleep_time)
-            while True:
-                self.sock.recv(23)  # clear buffer
-                self.sock.send("G_P".encode())
-                sleep(0.3)
-                recv = self.sock.recv(26).decode()
-                curr_x, curr_y, curr_z = self.get_coordinates(recv)
-                print("Current position: ", curr_x, " ", curr_y, " ", curr_z, " ")
-
-                offsets = []
-                offsets.append(abs(set_x - curr_x))
-                offsets.append(abs(set_y - curr_y))
-                offsets.append(abs(set_z - curr_z))
-
-                if max(offsets) <= self.offset_threshold:
-                    break
-                else:
-                    self.sock.send(command.encode())
-                sleep(self.sleep_time)
-
-        elif prefix == "TIM":
-            timeout = command[3:6]
-            sleep(int(timeout) // 1000)
-        elif prefix == "JNT":
-            pass
-
-        elif prefix == "CIR":
-            pass
-
-        sleep(self.sleep_time)
-        return
-
-    # todo update to enable controlling the suction cup and differentiate between picking up and putting down
-    def pick_up_command(self, coordinates):
-        pick_up = []
-        pick_up.append("LIN" + coordinates + self.obj_hover_height + "TOOL_")
-        pick_up.append("LIN" + coordinates + self.obj_pickup_height + "TOOL_")
-        pick_up.append("TIM" + str(2000))
-        pick_up.append("LIN" + coordinates + self.obj_hover_height + "TOOL_")
-        return pick_up
-
-    def putting_down_command(self, put_location):
-        put_down = []
-        put_down.append("LIN" + put_location + self.obj_hover_height + "TOOL_")
-        put_down.append("LIN" + put_location + self.obj_pickup_height + "TOOL_")
-        put_down.append("TIM" + str(2000))
-        put_down.append("LIN" + put_location + self.obj_hover_height + "TOOL_")
-        return put_down
-
-    def get_coordinates(self, s):
-        x = int(s[4:8]) if s[3] == "+" else int(s[3:8])
-        y = int(s[9:13]) if s[8] == "+" else int(s[8:13])
-        z = int(s[14:18]) if s[13] == "+" else int(s[13:18])
-        # todo convert to number normalize x, y, z
-        return x, y, z
-
-    def create_commands(self, x, y, type_of):
-        commands = []
-
-        x = "+" + str(x) if x >= 0 else str(x)
-        y = "+" + str(y) if y >= 0 else str(y)
-
-        # pick up object
-        commands.extend(self.pick_up_command(x + y))
-
-        # put down to specific location and go to default position
-        match type_of:
-            case 0:
-                commands.extend(self.putting_down_command(self.put_location_1))
-            case 1:
-                commands.extend(self.putting_down_command(self.put_location_2))
-            case 2:
-                commands.extend(self.putting_down_command(self.put_location_3))
-            case 3:
-                commands.extend(self.putting_down_command(self.put_location_4))
-
-        # return home command
-        commands.append("LIN" + self.home_pos + "TOOL_")
-        return commands
-
-    def start(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.HOST, self.PORT))
-        sleep(1)
-        print("Connected to delta")
-
-        # go home
-        self.execute_command("LIN" + self.home_pos + "TOOL_")
-        commands = []
-
-        # create commands to move every detected object
-        for element in queue:
-            if abs(element[0]) > 3000 or abs(element[1] > 3000):
-                continue
-            commands.extend(self.create_commands(element[0], element[1], element[2]))
-        print(commands)
-
-        while commands:
-            self.execute_command(commands[0])
-            commands.pop(0)
-# used to communicate with robot delta
+vision_stop = False
+vision_host, vision_port = "localhost", 8070
+vision_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    vision_sock.connect((vision_host, vision_port))
+    print("Connected to vision system server")
+except Exception as e:
+    print(e)
 
 
-# used to communicate with Vision System
-class VisonSystemClient(Singleton):
-    sock = None
-    running = False
-    HOST, PORT = "localhost", 8070
+# vision system handle
+def get_data():
+    global queue
+    global queue_lock
+    global vision_sock
+    vision_sock.recv(1024).decode()  # clear buffer
+    recv_str = vision_sock.recv(2048).decode()
 
-    def __init__(self):
-        if self.sock is None:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.running = True
+    # parse the input
+    splitted_str = recv_str.split("\n")
+    for s in splitted_str:
+        s_replaced = s.replace("\r", "")
 
-    def __del__(self):
-        if self.sock:
-            self.sock.close()
+        # compute only first valid input
+        if is_valid_json(s_replaced):
+            queue_lock.acquire()
+            queue = parse_data_from_string(s_replaced)
+            print("Updated queue with: ", queue)
+            queue_lock.release()
+            break
 
-    def start(self):
-        try:
-            self.sock.connect((self.HOST, self.PORT))
-            self.sock.recv(1024)  # clear buffer, skip header
-            print("Connected to server")
-        except Exception as e:
-            print(e)
 
-    def get_data(self):
-        self.sock.recv(1024).decode()  # clear buffer
-        recv_str = self.sock.recv(2048).decode()
-        # print("Recived data: ", recv_str)
+# vision system helper functions
+def parse_data_from_string(s_replaced):
+    dic = json.loads(s_replaced)
+    local_queue = []
+    for index, (name, detections) in enumerate(dic.items()):
+        if detections:
+            for detection in detections:
+                x, y = detection['middle_transformed']
+                # normalize x, y
+                if 0 <= x <= 416 and 0 <= x <= 416:
+                    x = x / 416
+                    y = y / 416
+                    local_queue.append((x, y, index))
 
-        # parse the input
-        splited_str = recv_str.split("\n")
-        for s in splited_str:
-            s_replaced = s.replace("\r", "")
+    return local_queue
 
-            # compute only first valid input
-            if self.is_valid_json(s_replaced):
-                self.parse_data_from_string(s_replaced)
+
+def is_valid_json(s):
+    if len(s) < 65:
+        return False
+    if s[0] != "{" or s[-1] != "}":
+        return False
+    return True
+
+
+"""
+    Init communication with robot delta
+"""
+
+# variables used to
+delta_host, delta_port = "localhost", 2137
+home_pos = "-2000-2000-4500"
+obj_hover_height = "-4500"
+obj_pickup_height = "-7000"
+offset_threshold = 100  # how much can differ the real and set position
+sleep_time = 1  # how long in seconds will the G_P command be send while checking if achieved position
+calibration_box_size = (6000, 6000)  # size of the square that is used when calibrating delta vision system
+
+# put down location coordinates
+put_location_1 = "+1000+1000"
+put_location_2 = "+1000+1000"
+put_location_3 = "+1000+1000"
+put_location_4 = "+1000+1000"
+
+delta_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    delta_sock.connect((delta_host, delta_port))
+    print("Connected with robot delta")
+except Exception as e:
+    print(e)
+
+
+def execute_command(command):
+    global delta_sock
+    return_value = None
+    prefix = command[0:3]
+
+    if prefix == "G_P":
+        delta_sock.send("G_P".encode())
+        recv = delta_sock.recv(23).decode()
+        return recv
+
+    elif prefix == "LIN":
+        print("Performing: ", command)
+        set_x, set_y, set_z = get_coordinates(command)
+        delta_sock.send(command.encode())
+        sleep(sleep_time)
+        while True:
+            delta_sock.recv(23)  # clear buffer
+            delta_sock.send("G_P".encode())
+            sleep(0.3)
+            recv = delta_sock.recv(26).decode()
+            curr_x, curr_y, curr_z = get_coordinates(recv)
+            print("Current position: ", curr_x, " ", curr_y, " ", curr_z, " ")
+
+            offsets = [abs(set_x - curr_x), abs(set_y - curr_y), abs(set_z - curr_z)]
+
+            # todo consider checking for moving
+            if max(offsets) <= offset_threshold:
                 break
+            sleep(sleep_time)
 
-        # endl = recv_str.find("\n")
-        # striped_str = recv_str[:endl - 1]
-        # print(striped_str, " ", type(striped_str))
-        # try:
-        #     json_data = json.loads(striped_str)
-        #     print("From Vision System: ",  json_data)
-        #
-        # except:
-        #     print("___")
-        #     pass
-        # if isinstance(striped_str, str) and striped_str[0] == "{" and striped_str[]:
-        #     json_data = json.loads(striped_str)
-        #     print("From Vision System: ",  json_data)
-        # else:
-        #     print("Not a string: ", striped_str)
-        # queue_lock.acquire()
+    elif prefix == "TIM":
+        timeout = command[3:6]
+        sleep(int(timeout) // 1000)
 
-    def is_valid_json(self, s):
-        if len(s) < 65:
-            return False
-        if s[0] != "{" or s[-1] != "}":
-            return False
-        return True
+    elif prefix == "JNT":
+        pass
+
+    elif prefix == "CIR":
+        pass
+
+    sleep(sleep_time)
+    return
 
 
-    def parse_data_from_string(self, s_replaced):
-        dic = json.loads(s_replaced)
-        local_queue = []
-        for index, (name, detections) in enumerate(dic.items()):
-            if detections:
-                for detection in detections:
-                    x, y = detection['middle_transformed']
-                    # normalize x, y
-                    if 0 <= x <= 416 and 0 <= x <= 416:
-                        x = x / 416
-                        y = y / 416
-                        local_queue.append((x, y, index))
+# todo update to enable controlling the suction cup and differentiate between picking up and putting down
+def pick_up_command(coordinates):
+    pick_up = ["LIN" + coordinates + obj_hover_height + "TOOL_",
+               "LIN" + coordinates + obj_pickup_height + "TOOL_", "TIM" + str(2000),
+               "LIN" + coordinates + obj_hover_height + "TOOL_"]
+    return pick_up
 
 
+def putting_down_command(put_location):
+    put_down = ["LIN" + put_location + obj_hover_height + "TOOL_",
+                "LIN" + put_location + obj_pickup_height + "TOOL_", "TIM" + str(2000),
+                "LIN" + put_location + obj_hover_height + "TOOL_"]
+    return put_down
+
+
+def create_commands(x, y, type_of):
+    commands = []
+
+    if x >= 0:
+        x = str(x)
+        missing = 4 - len(x)  # we want to achieve 4 digit format
+        x = "+" + "0" * missing + x
+    else:
+        x = str(x)[1:]
+        missing = 4 - len(x)
+        x = "-" + "0" * missing + x
+
+    if y >= 0:
+        y = str(y)
+        missing = 4 - len(y)  # we want to achieve 4 digit format
+        y = "+" + "0" * missing + y
+    else:
+        y = str(y)[1:]
+        missing = 4 - len(y)
+        y = "-" + "0" * missing + y
+
+    # pick up object
+    commands.extend(pick_up_command(x + y))
+
+    # put down to specific location and go to default position
+    match type_of:
+        case 0:
+            commands.extend(putting_down_command(put_location_1))
+        case 1:
+            commands.extend(putting_down_command(put_location_2))
+        case 2:
+            commands.extend(putting_down_command(put_location_3))
+        case 3:
+            commands.extend(putting_down_command(put_location_4))
+
+    # return home command
+    commands.append("LIN" + home_pos + "TOOL_")
+    return commands
+
+
+def sort(local_queue):
+    # create commands to move every detected object
+    commands = []
+    for element in local_queue:
+        # get x, y, normalize it regarding calib box size
+        x = int(element[0] * calibration_box_size[0] - calibration_box_size[0])
+        y = int(element[1] * calibration_box_size[1] - calibration_box_size[1])
+
+        if abs(x) > 3000 or abs(y) > 3000:
+            continue
+        commands.extend(create_commands(x, y, element[2]))
+    print(commands)
+
+    while commands:
+        execute_command(commands[0])
+        commands.pop(0)
+
+
+def get_coordinates(s):
+    x = int(s[4:8]) if s[3] == "+" else int(s[3:8])
+    y = int(s[9:13]) if s[8] == "+" else int(s[8:13])
+    z = int(s[14:18]) if s[13] == "+" else int(s[13:18])
+    return x, y, z
+
+
+"""
+    Program thread and loops
+"""
+
+
+def vision_system_loop():
+    while True:
+        get_data()
+
+# todo fix infite loop, set break conditions
+
+def delta_loop():
+    global queue
+    while True:
         queue_lock.acquire()
-        queue = local_queue
-        print(queue)
+        if queue:
+            sort(queue)
+            queue = []
+            break
+
         queue_lock.release()
+        sleep(3)
 
 
+th1 = threading.Thread(target=vision_system_loop)
+th1.start()
 
-d1 = VisonSystemClient()
-d1.start()
-while d1.running:
-    d1.get_data()
-    print("Current queue state: ", queue)
-    sleep(queue_update_time)  # how often should the queue be updated
+th2 = threading.Thread(target=delta_loop)
+th2.start()
 
-# d2 = DeltaClient()
-# d2.start()
+th1.join()
+th2.join()
+
+vision_sock.close()
+delta_sock.close()
+
