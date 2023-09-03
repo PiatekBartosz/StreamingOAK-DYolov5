57f0daef0a620a18153bf96cf8a6a06590ac9a03
PiatekBartosz <bartekpiatek123@gmail.com>
2023-09-04 00:27:38 +0200

    userinterface: updating a field working

--- helpers/userinterface.py
+++ helpers/userinterface.py
@@ -1,3 +1,67 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Static
+from textual.reactive import reactive
+from delta import SharedQueue
+import threading
+import time
+
+class QueueDisplay(Static):
+    def on_mount(self) -> None:
+        self.set_interval(1, self.update_queue)
+
+    def update_queue(self) -> None:
+        self.update(str(sq.get_queue()))
+
+
+
+class DeltaTextUserInterfaceApp(App):
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def __init__(self, sq):
+        super().__init__()
+        self.sq = sq
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield QueueDisplay("Test messege")
+
+    # toggle dark mode after pressing "d"
+    def action_toogle_dark(self) -> None:
+        self.dark = not self.dark
+
+    def on_mount(self) -> None:
+        self.title = "Robot Delta Text User Interface"
+
+class SharedQueueTest(SharedQueue):
+        def update_queue_periodically(self):
+            while True:
+                # Simulate updating the queue with new data (replace this with your actual data update logic)
+                new_data = time.time()
+                self.set_queue(new_data)
+                
+                # Sleep for a while before the next update
+                time.sleep(1)  # Update every 1 second (adjust as needed)
+
+
+# later will be put into app.py
+if __name__ == "__main__":
+    sq = SharedQueueTest()
+    th1 = threading.Thread(target=sq.update_queue_periodically)
+    th1.start()
+    time.sleep(2)
+    app = DeltaTextUserInterfaceApp(sq)
+    app.run()
+
+
+
+
+
+
+
+
+
 # import curses
 # import threading
 # import time
@@ -42,44 +106,44 @@
 #         self.running = False
 
 
-import os
-import sys
-from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Label, Button, Placeholder
-from helpers.delta import SharedQueue
-import threading
+# import os
+# import sys
+# from textual.app import App, ComposeResult
+# from textual.widgets import Header, Footer, Label, Button, Placeholder
+# from helpers.delta import SharedQueue
+# import threading
 
-class TextUserInterface(App):
+# class TextUserInterface(App):
 
-    BINDINGS = [
-        ("d", "toggle_dark", "Toggle dark mode"),
-        ("s", "start_sorting", "Start sorting process"),
-        ("q", "quit", "Quit App")
-    ]
+#     BINDINGS = [
+#         ("d", "toggle_dark", "Toggle dark mode"),
+#         ("s", "start_sorting", "Start sorting process"),
+#         ("q", "quit", "Quit App")
+#     ]
 
-    CSS_PATH = "textuserinterface.tcss"
+#     CSS_PATH = "textuserinterface.tcss"
 
-    def __init__(self, app):
-        super().__init__()
-        self.depthaiApp = app
-        self.value = ""
-        self.label_widget = Label("Test message")
+#     def __init__(self, app):
+#         super().__init__()
+#         self.depthaiApp = app
+#         self.value = ""
+#         self.label_widget = Label("Test message")
 
-    def compose(self):
-        yield Header(Placeholder)
-        yield Footer()
-        yield self.label_widget
+#     def compose(self):
+#         yield Header(Placeholder)
+#         yield Footer()
+#         yield self.label_widget
 
-    def update_label(self):
-        i = 0
-        while not self.should_exit:
-            i += 1
-            self.value = self.depthaiApp.shared_queue.get_queue()
-            self.label_widget.update(str(i))
-            time.sleep(1)
+#     def update_label(self):
+#         i = 0
+#         while not self.should_exit:
+#             i += 1
+#             self.value = self.depthaiApp.shared_queue.get_queue()
+#             self.label_widget.update(str(i))
+#             time.sleep(1)
 
-    def on_load(self, event):
-        threading.Thread(target=self.update_label, daemon=True).start()
+#     def on_load(self, event):
+#         threading.Thread(target=self.update_label, daemon=True).start()
 
 
     # def on_load(self, event):
